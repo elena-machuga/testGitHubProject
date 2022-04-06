6481e5d0dfc480487fa7075ba13d193d30d7e686
Elena Machuga <elena.machuga@gmail.com>
2022-04-06 15:46:51 +0300

    calculate and print perimeter

--- src/com/java/testgit/Main.java
+++ src/com/java/testgit/Main.java
@@ -4,5 +4,7 @@ public class Main {
      public static void main(String[] args) {
          Rectangle rectangle=new Rectangle(10,5);
          System.out.println(rectangle.calculateArea());
+         Rectangle rectangle1 = new Rectangle(15, 20);
+         System.out.println(rectangle1.calculatePerimeter());
      }
 }
